0b10812f5fd2ffa068a5f285edd2d58853014b0e
Nikita Berstnev <bignick666@yandex.ru>
2024-04-21 21:18:43 +0300

    added all

--- main.go
+++ main.go
@@ -2,53 +2,75 @@ package main
 
 import (
 	"crypto/sha256"
+	"database/sql"
 	"fmt"
 	"net/http"
 
-	"github.com/redis/go-redis/v9"
+	_ "github.com/lib/pq"
 )
 
-var redisdb *redis.Client
+type from_db_url struct {
+	url string
+}
 
-func AddtoDB() {
+func AddtoDB(url string, code string) {
+	connStr := "user=postgres password=Rkhg2s9121096 dbname=shortener sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
 
-	opt, err := redis.ParseURL("redis://localhost:6379/0")
+	res, err := db.Exec("INSERT INTO shortener (url, code) VALUES ($1, $2)", url, code)
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Print("Добавлено")
+	fmt.Println(res.RowsAffected())
+
+}
+
+func GetFromDB(code string) {
+	connStr := "user=postgres password=Rkhg2s9121096 dbname=shortener sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	row := db.QueryRow("SELECT url FROM shortener where code = $1", code)
+
+	answer := from_db_url{}
+	err = row.Scan(&answer.url)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println("addr is", opt.Addr)
-	fmt.Println("db is", opt.DB)
-	fmt.Println("password is", opt.Password)
-	//client := redis.NewClient(opt)
-
-	//_ := redis.NewClient(opt)
-
+	fmt.Println(answer.url)
 }
+
 func MakeShort(w http.ResponseWriter, r *http.Request) {
-	ss := "https://gobyexample.com"
-
-	resp, err := http.Get(ss)
-
-	if err != nil {
-		return
-	}
-
-	if resp.StatusCode != 200 {
-		fmt.Println(resp.StatusCode)
-	} else {
-		fmt.Printf("Status code of website: %d\n", resp.StatusCode)
-	}
 
 	url := r.URL.Query().Get("url")
 	hash := fmt.Sprintf("%x", sha256.Sum256([]byte(url)))[:10]
 
+	AddtoDB(url, string(hash))
+	fmt.Println("Dobavleno: ")
 	fmt.Printf("ssilka: %s\n", url)
 	fmt.Printf("hashed to: %s\n", hash)
 }
 
+func DecodeURL(w http.ResponseWriter, r *http.Request) {
+	hash_code := r.RequestURI[3:len(r.RequestURI)]
+
+	fmt.Printf("hash is :%s\n", hash_code)
+	fmt.Println("Url is: ")
+	GetFromDB(hash_code)
+}
+
 func main() {
-	AddtoDB()
+	http.HandleFunc("/s/", DecodeURL)
 	http.HandleFunc("/a/", MakeShort)
 	http.ListenAndServe(":8000", nil)
 
