d20ed22d7e1317d15fb2ce98a7625abf782ac978
Nikita Berstnev <bignick666@yandex.ru>
2024-04-24 19:13:20 +0300

    dunno

--- main.go
+++ main.go
@@ -13,39 +13,22 @@ type from_db_url struct {
 	url string
 }
 
-func ConnecttoDB() *sql.DB {
-	connStr := "user=postgres password=Rkhg2s9121096 dbname=shortener sslmode=disable"
-	db, err := sql.Open("postgres", connStr)
-	if err != nil {
-		panic(err)
-	}
-	return db
-}
-
-// Добавил вот это
-func CloseDB(db *sql.DB) {
-	rows, err := ConnecttoDB().Query("SELECT *, pg_terminate_backend(pid) FROM pg_stat_activity WHERE pid <> pg_backend_pid() AND datname = 'shortener'")
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(rows)
-}
+var database *sql.DB
 
 func AddtoDB(url string, code string) {
-	res, err := ConnecttoDB().Exec("INSERT INTO shortener (url, code) VALUES ($1, $2)", url, code)
+	res, err := database.Exec("INSERT INTO shortener (url, code) VALUES ($1, $2)", url, code)
 
 	if err != nil {
 		panic(err)
 	}
 	fmt.Print("Добавлено")
 	fmt.Println(res.RowsAffected())
-	CloseDB(ConnecttoDB())
 
 }
 
 func GetFromDB(code string) {
 
-	row := ConnecttoDB().QueryRow("SELECT url FROM shortener where code = $1", code)
+	row := database.QueryRow("SELECT url FROM shortener where code = $1", code)
 
 	answer := from_db_url{}
 	err := row.Scan(&answer.url)
@@ -54,7 +37,18 @@ func GetFromDB(code string) {
 		fmt.Println(err)
 	}
 	fmt.Println(answer.url)
-	CloseDB(ConnecttoDB())
+	// kol-vo konnektov - 11
+	/*
+
+		var count int
+		errik := database.QueryRow("SELECT COUNT(*) FROM pg_stat_activity").Scan(&count)
+
+		if errik != nil {
+			fmt.Println(errik)
+		}
+		fmt.Printf("There are %d connections in the database\n", count)
+	*/
+
 }
 
 func MakeShort(w http.ResponseWriter, r *http.Request) {
@@ -71,13 +65,21 @@ func MakeShort(w http.ResponseWriter, r *http.Request) {
 func DecodeURL(w http.ResponseWriter, r *http.Request) {
 	hash_code := r.RequestURI[3:len(r.RequestURI)]
 
-	fmt.Printf("hash is :%s\n", hash_code)
+	fmt.Printf("hash is: %s\n", hash_code)
 	fmt.Println("Url is: ")
 	GetFromDB(hash_code)
 
 }
 
 func main() {
+	connStr := "user=postgres password=Rkhg2s9121096 dbname=shortener sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		panic(err)
+	}
+	database = db
+	defer db.Close()
+
 	http.HandleFunc("/s/", DecodeURL)
 	http.HandleFunc("/a/", MakeShort)
 	http.ListenAndServe(":8000", nil)
